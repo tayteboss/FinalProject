117926dd8948f3cfa1c2bca8d65299e03ca124b6
Tayte Boss <tayte.boss@outlook.com>
2018-05-30 18:43:03 +1000

    done

--- js/main.js
+++ js/main.js
@@ -2,35 +2,65 @@
 
 
 
-$(document).ready(activePage)
 $(document).ready(fadeIn)
-$('.artist').click(pageArtist)
-$('.visual').click(visual)
-
-
-function activePage() {
-  var pageis = $('body').attr('data-page')
-  $('a.' + pageis).addClass('activeNav' + pageis)
-
-}
+$('.artistToggle').click(artistToggle)
+$('.navLink').click(pageChange)
+$('.mobileNav').click(mobileNavToggle)
 
 function fadeIn() {
+  // triggers animation delays
   $(".animate").addClass('make-visible')
   $(".logoAnimate").addClass('make-width')
 }
 
-function pageArtist() {
-  event.preventDefault()
-  $('main').css('display', 'none')
-  $('.mainArtist').css('display', 'flex')
-
-  $('.navBar .a').removeClass('activeNav' + 'about')
-  $('a.artist').addClass('activeNav' + 'artist')
+function artistToggle() {
+// Variable of what's been clicked
+  var tab = $(this).attr('data-target')
+  $("article").css("display", "none")
+  $(".animate").removeClass('make-visible')
+  // fades in article based on what was clicked
+  $('.' + tab).fadeIn(0);
+  // sets slight delay so articles aren't fading in & out on each other
+  setTimeout(function(){
+    $(".animate").addClass('make-visible')
+  }, 50);
 }
 
-function visual() {
-  $('.artistSound').css('display', 'none')
-  $('artistVisual').css('artistVisualOn')
+function pageChange() {
+  // Variable of what's been clicked
+  var content = $(this).attr('data-target')
+  // removing longer delays and adding shorter delays for later page changes
+  $('.content-section .delay-1400').removeClass('delay-1400').addClass('delay-200')
+  $('.content-section .delay-1600').removeClass('delay-1600').addClass('delay-400')
+  $('.content-section .delay-1800').removeClass('delay-1800').addClass('delay-600')
+  $('.content-section .delay-2000').removeClass('delay-2000').addClass('delay-800')
+  // shows active page
+  $('.navLink').removeClass('activeNav')
+  $(this).addClass('activeNav')
+  // fades out & in appropriate main sections
+  $('.content-section').fadeOut(0)
+  $('.content-section').removeClass('active-section');
+  $(".animate").removeClass('make-visible')
+  $('#' + content).fadeIn(0);
+  // Adds (active-section) class for mobile menu section
+  $('#' + content).addClass('active-section');
+  setTimeout(function(){
+    $(".animate").addClass('make-visible')
+  }, 200);
 
+// ensures nav bar fades out when clicked
+  if ( $(window).width() < 631 ) {
+    $('.mobileNav').toggleClass('change')
+    $('.navBar').fadeOut(300)
+  }
 }
 
+  function mobileNavToggle() {
+    $(this).toggleClass('change')
+    $('.content-section').fadeOut(300)
+    if (!$(this).hasClass('change')) {
+      // fades in active-section
+      $('.content-section.active-section').fadeIn(300)
+    }
+    $('.navBar').fadeToggle(300)
+}
